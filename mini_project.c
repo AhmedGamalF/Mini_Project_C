ff1fbb85cce267cb2d09f5d29f97351cccce39c5
Ahmed_Gamal <133501319+AhmedGamalF@users.noreply.github.com>
2023-06-07 01:36:03 +0300

    Update mini_project.c

--- mini_project.c
+++ mini_project.c
@@ -1,263 +1,257 @@
-/****************************************************************************************
- *   Name: Ahmed Gamal Abdelrahman Nasr
- *
- *   Describtion : Mini_project.c
- ***************************************************************************************/
+/*
+ ============================================================================
+ Name        : Mini_Project1.c
+ Author      : Mohamed Tarek
+ Description : Vehicle Control System Project
+ Date        : 01/09/2021
+ ============================================================================
+ */
 
-# include <stdio.h>
+#include <stdio.h>
 
-# define Display_size 3 // size of the frist array to the 3 selections
-# define sensor_size 4 // size of the second array to the 4 selections
+/*** Requirement 9 ***/
+#define WITH_ENGINE_TEMP_CONTROLLER 1
 
-/* defintion of Motor state */
-enum motor_state {
-	ON = 1 , OFF = 0
-};
-
-/* VEHICLE DATA*/
-struct vehicle
+typedef enum
 {
-	int Engine ;
-	int AC ;
-	int vehicle_speed ;
-	int Room_temp ;
-	int Engine_temp_control ;
-	int Engine_temp ;
-} vehicle_data ;
+	AC_OFF, AC_ON
+}Air_Condion_State;
 
-
-char Display_system (char *p1);
-char sensor_set_menu(char *index);
-void traffic_light_chose(char color);
-void Room_temprature (int deg);
-void Engine_controller(int temp);
-void Print_current_state(void);
-
-
-/* main function */
-int main (void)
+typedef enum
 {
-	char array [Display_size] = { 'a', 'b' , 'c' };
-	char sensor[sensor_size] = { 'a', 'b' , 'c' , 'd' };
-	char select = Display_system (array); // call function to start system
-	char menu ;
+	CONTROLLER_OFF, CONTROLLER_ON
+}Engine_Temperature_Controller_State;
 
-	/* Vehicle intial Data  */
-	vehicle_data.Engine = ON ;
-	vehicle_data.AC = OFF;
-	vehicle_data.vehicle_speed = 90 ;
-	vehicle_data.Room_temp = 32 ;
-	vehicle_data.Engine_temp_control = OFF;
-	vehicle_data.Engine_temp = 80 ;
+typedef enum
+{
+	ENGINE_OFF, ENGINE_ON
+}Engine_State;
 
-	//user choose to turn on the Engine
-	while (select == 'a')
+Air_Condion_State Vehicle_SetupRoomTemp(int *temp_ptr)
+{
+	Air_Condion_State State = AC_OFF;
+	if(*temp_ptr < 10)
 	{
-		printf("\n");
-		menu = sensor_set_menu(sensor); /* call sensor set menu function */
-		if (menu == 'a') // user choose to turn off the engine
+		State = AC_ON;
+		*temp_ptr = 20;
+	}
+	else if(*temp_ptr > 30)
+	{
+		State = AC_ON;
+		*temp_ptr = 20;
+	}
+	else
+	{
+		State = AC_OFF;
+	}
+	return State;
+}
+
+int Vehicle_CalculateSpeed(char Light)
+{
+	int Speed;
+
+	if((Light == 'G') || (Light == 'g'))	Speed = 100;
+	if((Light == 'O') || (Light == 'o'))    Speed = 30;
+	if((Light == 'R') || (Light == 'r'))	Speed = 0;
+
+	return Speed;
+}
+
+Engine_Temperature_Controller_State Vehicle_SetupEngineTempController(int *temp_ptr)
+{
+	Engine_Temperature_Controller_State State = CONTROLLER_OFF;
+	if(*temp_ptr < 100)
+	{
+		State = CONTROLLER_ON;
+		*temp_ptr = 125;
+	}
+	else if(*temp_ptr > 150)
+	{
+		State = CONTROLLER_ON;
+		*temp_ptr = 125;
+	}
+	else
+	{
+		State = CONTROLLER_OFF;
+	}
+	return State;
+}
+
+int main(void)
+{
+	/* For printf eclipse issue */
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+
+	char User_Input;
+	char Sensors_States;
+	char Traffic_Light;
+
+	Engine_State Engine = ENGINE_OFF;
+	Air_Condion_State AC_State = AC_OFF;
+	int Vehicle_Speed = 0;
+	int Temp_Room = 35;
+
+#if (WITH_ENGINE_TEMP_CONTROLLER == 1) /*** Requirement 9 ***/
+	Engine_Temperature_Controller_State Engine_Temp_Control = CONTROLLER_OFF;
+	int Temp_Engine = 90;
+#endif
+
+    for(;;)
+    {
+		/********************************** Requirement 1 **********************************/
+		printf("a. Turn on the vehicle engine\n");
+		printf("b. Turn off the vehicle engine\n");
+		printf("c. Quit the system\n\n");
+		/***********************************************************************************/
+
+		/* Get input from user (a,A) or (b,B) or (c,C) */
+		scanf(" %c",&User_Input);
+
+		/********************************** Requirement 2 and 4 ****************************/
+		/* if the input is 'c' or 'C' the system should be terminated */
+		if((User_Input == 'c') || (User_Input == 'C'))
 		{
-			select = Display_system (array); // return to start screen
+			printf("Quit The System \n\n");
+			break;
 		}
-	}
+		/***********************************************************************************/
 
-	/* if the user choose to turn off the Engine */
-	while ( select == 'b')
-	{
-		printf("\n");
-		select = Display_system (array);
-	}
-	return 0 ;
-}
-
-/**************************Display function ********************************************/
-
-//  function take an input from user to trun the engine on/off or quit system
-char Display_system (char *p1)
-{
-	char input = ' ';
-	printf("a. Trun on the vehicle engine.\n");
-	printf("b. Trun off the vehilce engine.\n");
-	printf("c. Quit the system\n");
-	fflush(stdout);
-	scanf(" %c" , &input); // read input
-	// user chose : trun on the engine
-	if(input == p1[0])
-	{
-		printf("a. Trun on the vehicle engine.\n");
-
-	}
-
-	// user chose : trun off the engine
-	if(input == p1[1])
-	{
-		printf("b. Trun off the vehilce engine.\n");
-
-	}
-
-	// user chose : quit
-	if(input == p1[2])
-	{
-		printf("Quit the system.\n");
-	}
-
-	return input ;
-}
-
-/*********************************Sensor menu function*********************************/
-
-/* function that select the sensor set menu */
-char sensor_set_menu(char *p2)
-{
-	char sensor_set ;
-	printf("a. Turn off the engine.\n");
-	printf("b. set the traffic light color.\n");
-	printf("c. set the room temprature.(tempreature sensor)\n");
-	printf("d. set the engine temprature.(Engine temprature sensor)\n");
-	fflush(stdout);
-	//read input from user to select  from the menu
-	scanf(" %c" ,&sensor_set);
-
-	/***********************************Select sensor type ********************/
-
-	/* choose traffic light */
-	if (sensor_set == p2[1])
-	{
-		char color ;
-		printf("Enter the requierd Color : \n");
-		fflush(stdout);
-		scanf(" %c" ,&color); // read color
-		if ((color =='g')||(color== 'o')||(color== 'r'))
+		/********************************** Requirement 3 and 4 ****************************/
+		/* if the input is 'b' or 'B' Turn OFF Vehicle Engine */
+		else if((User_Input == 'b') || (User_Input == 'B'))
 		{
-			traffic_light_chose(color); // call traffic light function
+			Engine = ENGINE_OFF;
+			printf("Turn oFF Vehicle Engine\n\n");
 		}
-	}
-	/*chose room temprature */
-	if (sensor_set == p2[2])
-	{
-		int degree ;
-		printf("Enter the Room temprature:\n");
-		fflush(stdout);
-		scanf(" %d" , &degree);//read degree
-		Room_temprature (degree); // call room temprature function
-	}
+		/***********************************************************************************/
 
-	/* Engine Temprature */
-	if (sensor_set == p2[3])
-	{
-		int engine_temp ;
-		printf("Enter the Engine temprature:\n");
-		fflush(stdout);
-		scanf(" %d" , &engine_temp); //read engine temprature
-		Engine_controller(engine_temp); // call engine controller function
-	}
+		/********************************** Requirement 5 and 6 ****************************/
+		else if((User_Input == 'a') || (User_Input == 'A'))
+		{
+			for(;;)
+			{
+				Engine = ENGINE_ON; /* Turn ON Vehicle Engine */
 
-	return sensor_set ;
+				printf("a. Turn off the engine\n");
+				printf("b. Set the traffic light color\n");
+				printf("c. Set the room temperature(Temperature Sensor)\n");
+
+#if (WITH_ENGINE_TEMP_CONTROLLER == 1) /*** Requirement 9 ***/
+				printf("d. Set the engine temperature\n\n");
+#endif
+				/***********************************************************************************/
+				scanf(" %c",&Sensors_States);
+
+				/********************************** Requirement 8 **********************************/
+				if((Sensors_States == 'a') || (Sensors_States == 'A'))
+				{
+					Engine = ENGINE_OFF; /* Turn OFF Vehicle Engine */
+					/* Break the inner for loop and go to Requirement 1 */
+					break;
+				}
+
+
+				/********************************** Requirement 7-a ********************************/
+				/* For Traffic_Light Light */
+				if((Sensors_States=='b') || (Sensors_States=='B'))
+				{
+					printf("Enter the required color:\n");
+					scanf(" %c",&Traffic_Light);
+
+					Vehicle_Speed = Vehicle_CalculateSpeed(Traffic_Light);
+				}
+				/***********************************************************************************/
+
+				/********************************** Requirement 7-b ********************************/
+				/* For Room Temperature */
+				if((Sensors_States == 'c') || (Sensors_States == 'C'))
+				{
+					printf("Enter the required room temp:\n");
+					scanf("%d",&Temp_Room);
+
+					/* Update AC State and Temp_Room */
+					AC_State = Vehicle_SetupRoomTemp(&Temp_Room);
+				}
+				/************************************************************************************/
+
+				/********************************** Requirement 7-c *********************************/
+				/* For Engine Temperature */
+#if (WITH_ENGINE_TEMP_CONTROLLER == 1) /*** Requirement 9 ***/
+				if((Sensors_States == 'd') || (Sensors_States == 'D'))
+				{
+					printf("Enter the required engine temperature:\n");
+					scanf("%d",&Temp_Engine);
+
+					/* Update Engine Temp Controller State and Temp_Engine */
+					Engine_Temp_Control = Vehicle_SetupEngineTempController(&Temp_Engine);
+				}
+#endif
+				/***********************************************************************************/
+
+				/********************************* Requirement 7-d *********************************/
+				if(Vehicle_Speed == 30)
+				{
+					if(AC_State == AC_OFF)
+					{
+						AC_State = AC_ON;
+					}
+					Temp_Room = ((Temp_Room * 5) / 4) + 1;
+
+#if (WITH_ENGINE_TEMP_CONTROLLER == 1) /*** Requirement 9 ***/
+					if(Engine_Temp_Control == CONTROLLER_OFF)
+					{
+						Engine_Temp_Control = CONTROLLER_ON;
+					}
+					Temp_Engine = ((Temp_Engine * 5) / 4) + 1;
+#endif
+				}
+				/***********************************************************************************/
+
+				/*********************************** Requirement 7-e *******************************/
+				if(Engine == ENGINE_OFF)
+				{
+					printf("Engine is OFF\n");
+				}
+				else
+				{
+					printf("Engine is ON\n");
+				}
+				if(AC_State == AC_OFF)
+				{
+					printf("AC is OFF\n");
+				}
+				else
+				{
+					printf("AC is ON\n");
+				}
+
+				printf("Vehicle Speed: %d Km/Hr\n",Vehicle_Speed);
+
+				printf("Room Temperature: %d C\n",Temp_Room);
+
+#if (WITH_ENGINE_TEMP_CONTROLLER == 1) /*** Requirement 9 ***/
+				if(Engine_Temp_Control == CONTROLLER_OFF)
+				{
+					printf("Engine Temp Controller is OFF\n");
+				}
+				else
+				{
+					printf("Engine Temp Controller is ON\n");
+				}
+				printf("Engine Temperature: %d C\n\n",Temp_Engine);
+#endif
+				/************************************************************************************/
+
+			}
+		}
+		else
+		{
+			printf("Error please enter valid input (a,A) or (b,B) or (c,C) \n\n");
+		}
+	} /* outer for loop */
+
+    return 0;
 }
 
-/*********************************Traffic function**************************************/
-
-/* function that select the traffic light color*/
-void traffic_light_chose(char color)
-{
-	/* if traffic light is green */
-	if( color == 'g')
-	{
-		vehicle_data .vehicle_speed	= 100 ;
-	}
-	/*traffic light is orange */
-	if( color == 'o')
-	{
-		vehicle_data .vehicle_speed	= 30 ;
-	}
-	/*trafic light is red*/
-	if( color == 'r')
-	{
-		vehicle_data .vehicle_speed	= 0 ;
-	}
-	Print_current_state(); // print final state
-
-}
-
-/*********************************Sensor room temptrature function**********************/
-void Room_temprature (int deg)
-{
-	if ((deg < 10 )|| (deg > 30 ))
-	{
-		vehicle_data.Room_temp = 20 ;
-		vehicle_data .AC = ON;
-	}
-	/* if temprature is greater than 10 and less than 30 trun off the AC */
-	else
-	{
-		vehicle_data .AC = OFF;
-	}
-	Print_current_state(); // print final state
-
-}
-
-/*********************************Engine temptrature function***************************/
-void Engine_controller(int temp)
-{
-	if ((temp < 100 )|| (temp > 150 ))
-	{
-		vehicle_data .Engine_temp = 125;
-		vehicle_data .Engine_temp_control= ON ;
-	}
-	/* if temprature is greater than 10 and less than 30 trun off the AC */
-	else
-	{
-		vehicle_data .Engine_temp_control= OFF ;
-	}
-	Print_current_state(); //print final state
-
-}
-/* print function */
-void Print_current_state(void)
-{
-
-	/********************************* Display the output **********************************/
-	/* special case  when vehicle speed is 30 Km/h set the follwing data*/
-	if(vehicle_data.vehicle_speed == 30) // it means that the traffic light is orange
-	{
-		vehicle_data.AC = ON;
-		vehicle_data.Room_temp = vehicle_data.Room_temp * (1.25) + 1 ;
-		vehicle_data.Engine_temp_control = ON;
-		vehicle_data.Engine_temp = vehicle_data.Engine_temp * (1.25) + 1 ;
-	}
-	if (vehicle_data.Engine == ON)
-	{
-		printf("Engine state is: ON\n");
-	}
-	else
-	{
-		printf("Engine state is : OFF\n");
-
-	}
-
-	if (vehicle_data.AC == ON)
-	{
-		printf("AC is : ON\n");
-	}
-	else
-	{
-		printf("AC is : OFF\n");
-
-	}
-
-	printf("Vehicle Speed:  %d Km/h\n" ,vehicle_data.vehicle_speed);
-	printf("Room Temperature:  %d C\n" ,vehicle_data.Room_temp);
-	switch (vehicle_data.Engine_temp_control)
-	{
-	case ON :
-		printf("Engine Temperature Controller State: ON\n");
-		break ;
-	case OFF :
-		printf("Engine Temperature Controller State: OFF\n");
-		break;
-	}
-
-	printf("Engine Temperature:  %d C\n" ,vehicle_data.Engine_temp);
-
-
-}
